409eb9bd078111a93604a531dfaaaf267436581b
Momcilo Mikovic <miiikovic19@gmail.com>
2023-01-25 17:59:43 +0100

    deploying

--- draganddrop-demo/src/app/app.module.ts
+++ draganddrop-demo/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { HomePageComponent } from './home-page/home-page.component';
 
+
 import { environment } from 'src/environments/environment';
 
 import { AngularFireModule } from "@angular/fire/compat";
--- draganddrop-demo/src/app/home-page/home-page.component.ts
+++ draganddrop-demo/src/app/home-page/home-page.component.ts
@@ -1,15 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
+  animations: [
+    trigger('fadeInFromTop', [
+      transition(':enter', [
+        style({
+          transform: 'translateY(-100%)',
+          opacity: 0
+        }),
+        animate('500ms ease-in', style({
+          transform: 'translateY(0)',
+          opacity: 1
+        }))
+      ])
+    ]),
+    trigger('fadeInfromLeft', [
+      transition(':enter', [
+        style({
+          transform: 'translateX(-100%)',
+          opacity: 0
+        }),
+        animate('500ms ease-in', style({
+          transform: 'translateY(0)',
+          opacity: 1
+        }))
+      ])
+    ]),
+    trigger('fadeInfromRight', [
+      transition(':enter', [
+        style({
+          transform: 'translateX(100%)',
+          opacity: 0
+        }),
+        animate('500ms ease-in', style({
+          transform: 'translateX(0)',
+          opacity: 1
+        }))
+      ])
+    ]),
+    trigger('fadeInfromBottom', [
+      transition(':enter', [
+        style({
+          transform: 'translateY(100%)',
+          opacity: 0
+        }),
+        animate('500ms ease-in', style({
+          transform: 'translateY(0)',
+          opacity: 1
+        }))
+      ])
+    ]),
+
+  ]
 })
 export class HomePageComponent implements OnInit {
+  show = false;
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  get stateName() {
+    return this.show ? 'show' : 'hide'
+  }
 
   ngOnInit(): void {
+    this.show = true;
+  }
+
+  navigateLogin() {
+    this.router.navigate(['/login']);
   }
 
 }
